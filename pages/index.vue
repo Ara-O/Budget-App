601118fa70c62ce87f94189fe00567947d8bb112
Ara <oladipoeyiara@gmail.com>
2022-06-17 09:18:49 -0400

    Wireframing main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,54 @@
 <template>
-<main class="index-main" v-if="userIsSignedIn">
-  <h3>Hello</h3>
-</main>
+  <main class="index-main" v-if="userIsSignedIn">
+    <section class="main-section_left">
+      <section class="income-section">
+        <h3>Income</h3>
+      </section>
+      <section class="bill-section">
+        <h3>Bills</h3>
+      </section>
+      <section class="total-income-section">
+        <h3>Total Income</h3>
+      </section>
+    </section>
+    <section class="main-section_right">
+      <section class="graph-section">
+        <h3>Graph section</h3>
+      </section>
+      <section class="expense-section">
+        <h3>Expense section</h3>
+      </section>
+    </section>
+  </main>
 </template>
 
 <script lang="ts">
-import {userIsSignedIn} from "../modules/firebaseServices.js";
-import Vue from 'vue'
+import { userIsSignedIn } from "../modules/firebaseServices.js";
+import Vue from "vue";
 export default Vue.extend({
-  head(){
+  head() {
     return {
-      title: "Save Money!"
-    }
-  }, 
-  data(){
+      title: "Save Money!",
+    };
+  },
+  data() {
     return {
       userIsSignedIn: false,
-    }
+    };
   },
-  mounted(){
-      userIsSignedIn().then((res)=> {
-        if(res){
-          this.userIsSignedIn = true;
-        }else {
-          this.$router.push("/signup")
-        }
-      })
-  }
-})
+  mounted() {
+    userIsSignedIn().then((res) => {
+      if (res) {
+        this.userIsSignedIn = true;
+      } else {
+        this.$router.push("/signup");
+      }
+    });
+  },
+});
 </script>
+
+<style>
+@import url("../assets/styles/styles.css");
+@import url("../assets/styles/main.css");
+</style>
